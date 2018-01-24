b2bc90c68214d09d7efa53e959ef68f954e1e112
Jacob Evans <jacob@dekz.net>
2018-01-25 09:10:21 +1100

    create an index.ts

---
+++ lib/index.ts
@@ -0,0 +1,9 @@
+import { ZeroEx } from '0x.js';
+import { InjectedWeb3Subprovider } from '@0xproject/subproviders';
+import { promisify } from '@0xproject/utils';
+import * as Web3 from 'web3';
+console.log(ZeroEx);
+console.log(Web3);
+console.log('aaa');
+console.log('aaaa');
+
